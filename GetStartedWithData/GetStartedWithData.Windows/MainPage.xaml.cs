2631169890a38873d31f60504bcd7acd7ecf9f16
Yuqi Qian <yuqi.qian@outlook.com>
2015-09-11 18:39:16 -0400

    Initialization

---
+++ GetStartedWithData/GetStartedWithData.Windows/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace GetStartedWithData
+{   
+
+    public sealed partial class MainPage : Page
+    {
+    }
+
+    
+}
---
+++ GetstartedwithdataYuqiMobileService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace GetstartedwithdataYuqiMobileService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
